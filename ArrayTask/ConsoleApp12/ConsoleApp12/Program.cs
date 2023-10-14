978fe41a1fa4b2a6fac561f0d7871cdfe502e4f9
unknown <fuadgi@code.edu.az>
2023-10-15 02:16:55 +0400

    ArrayTask

---
+++ ArrayTask/ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,46 @@
+﻿#region task1
+//int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//int result = 0;
+//foreach (int number in numbers)
+//{
+//    result += number;
+
+//}
+//Console.WriteLine(result);
+#endregion
+#region task2
+//int[] numbers = { 1, 2, 3, 60, 59, 30, 20, 13, 12, 10 };
+//int result = 0;
+//foreach (int number in numbers)
+//{
+//    if (number >= 10)
+//    {
+//        result++;
+//    }
+//}
+//Console.WriteLine(result);
+
+
+#endregion
+#region task3
+//int[] numbers = { 1, 2, 3, 60, 59, 30, 20, 13, 12, 10 };
+//int result = 0;
+//foreach (int number in numbers)
+//{
+//    result += number;
+
+//}
+//Console.WriteLine(result / numbers.Length) ;
+#endregion
+#region task4
+//string[] aylar = { "yanvar", "fevral", "mart", "aprel", "may", "iyun", "iyul", "avqust", "sentyabr", "oktyabr", "noyabr", "dekabr" };
+
+//foreach (string yaz in aylar)
+//{
+//    if (yaz == "mart" || yaz == "aprel" || yaz == "may")
+//    {
+//        Console.WriteLine(yaz);
+//    }
+
+//}
+#endregion
